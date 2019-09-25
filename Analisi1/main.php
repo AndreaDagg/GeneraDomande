85ac1fa89d3bf9a17361fc6af8a050777a46abf3
AndreaDagg <daguannoandrea@outlook.com>
2019-09-25 14:19:38 +0200

    Note3

--- Analisi1/main.php
+++ Analisi1/main.php
@@ -67,7 +67,7 @@ if (!isset($_SESSION['NumIndex'])) {
             </div>
             <div class="row">
                 <div class="col">
-                    <button id="btnARG" type="submit" class="btn btn-outline-warning" name="arg" value="2"><h4>
+                    <button id="btnARG" type="submit" class="btn btn-outline-secondary" name="arg" value="2"><h4>
                             Derivate</h4>
                     </button>
                 </div>
@@ -78,7 +78,7 @@ if (!isset($_SESSION['NumIndex'])) {
                  </div>-->
 
                 <div class="col">
-                    <button id="btnARG" type="submit" class="btn btn-outline-success" name="arg" value="5"><h4>Eq.
+                    <button id="btnARG" type="submit" class="btn btn-outline-primary" name="arg" value="5"><h4>Eq.
                             Differenziali</h4>
                     </button>
                 </div>
@@ -132,53 +132,49 @@ if (!isset($_SESSION['NumIndex'])) {
     ?>
 
 
-    <div class="row">
-        <div class="d-flex justify-content-center">
-            <form id="close" action="https://www.google.it">
-                <div class="col">
-                    <button id="btnARG" type="submit" class="btn btn-success" value="ciao">
-                        <i class="material-icons">
-                            <h1>search</h1>
-                        </i>
-                        <p> Ricerca Google</p>
-                    </button>
-                </div>
-            </form>
-            <form id="close" action="../index.php">
-                <div class="col">
-                    <button id="btnSe" type="submit" class="btn btn-danger">
-                        <i class="material-icons">
-                            <h1>close</h1>
-                        </i>
-                        <p>Home</p>
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <div class="card" id="endCard">
-        <div class="container" id="endCardText">
-            <div class="d-flex justify-content-center">
-                <i class="material-icons">
-                    <h5 style="margin-right: 17px;color: lightgrey">share</h5>
-                </i>
-                <a href="https://github.com/AndreaDagg/GeneraDomandeAnalisi1.git" class="card-text"><h5
-                            style=" font-family: 'Comic Sans MS'; color: lightgrey"> GitHub </h5></a>
+    <div class="d-flex justify-content-center">
+        <form id="close" action="https://www.google.it">
+            <div class="col">
+                <button id="btnARG" type="submit" class="btn btn-outline-success" value="ciao">
+                    <i class="material-icons">
+                        <h1>search</h1>
+                    </i>
+                    <p> Ricerca Google</p>
+                </button>
             </div>
-        </div>
+        </form>
+        <form id="close" action="../index.php">
+            <div class="col">
+                <button id="btnSe" type="submit" class="btn btn-outline-danger">
+                    <i class="material-icons">
+                        <h1>close</h1>
+                    </i>
+                    <p>Home</p>
+                </button>
+            </div>
+        </form>
     </div>
+</div>
+<div id="endCard">
+    <div class="d-flex justify-content-center" id="endCardText">
+        <i class="material-icons">
+            <h5 style="margin-right: 17px;color: lightgrey">share</h5>
+        </i>
+        <a href="https://github.com/AndreaDagg/GeneraDomandeAnalisi1.git" class="card-text"><h5
+                    style=" font-family: 'Comic Sans MS'; color: lightgrey"> GitHub </h5></a>
+    </div>
+</div>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
 
