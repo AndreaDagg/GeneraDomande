fdd91aa23c9d10a39802c1b19d43497e467a379c
Andrea D'Aguanno <41380457+AndreaDagg@users.noreply.github.com>
2019-09-27 22:54:17 +0200

    Update note.php

--- Analisi1/note.php
+++ Analisi1/note.php
@@ -3,12 +3,14 @@ session_start();
 
 $_SESSION['NumIndex'] = 0;
 $NotePass = Array("");
-$NoteCos = Array("Limiti" => array("Dimostrazione e^x - 1 / x"), "Successioni" => array("Dimostrazione 1^infinity", 
-                           "Dimostrazione n(n+1)/2", "Dimostrazione e"), "Derivate"
+$NoteCos = Array("Limiti" => array("Dimostrazione e^x - 1 / x"), 
+"Successioni" => array("Dimostrazione 1^infinity", 
+                           "Dimostrazione n(n+1)/2", "Dimostrazione e"), 
+"Derivate"
                  => array("Dimostrazione Continuita' e derivabilita'", "Derivata F^-1 & arcoseno ...","Dimostrazione Fermat",
                          "Dimostrazione Lagrange","REsto di Peano e Lagrange"),
-    "Serie" => array("1/4 -> 1/3 SerArm"), "Integrali"=>("Pass. Th.Fondamentale del calcolo"));
-
+"Serie" => array("1/4 -> 1/3 SerArm"), 
+"Integrali" => array("Pass. Th.Fondamentale del calcolo"));
 $_SESSION['NumIndex'] = $NoteCos;
 
 print_r($_SESSION['NumIndex']);
