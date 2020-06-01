2bb2c55c9694e742527daa527c698249e682a30a
AndreaDagg <daguannoandrea@outlook.com>
2020-06-01 18:51:25 +0200

    FixAnd

--- Algoritmi/noteAlgoritmi.php
+++ Algoritmi/noteAlgoritmi.php
@@ -5,8 +5,10 @@ session_start();
 
 
 $_SESSION['NumIndex'] = 0;
-$NotePass = Array("");
-$NoteCos = Array("Strutture Dati" => array("..."), "Grafi" => array("..."), "..." => array("..."), "..." => array("..."));
+$NotePass = array("");
+$NoteCos = array("Strutture Dati" => array("..."), "Grafi" => array("..."), "Alberi" => array("..."),
+    "Complessita' & Analisi" => array("..."), "Ordinamento" => array("..."),
+    "Prog. Dinamica" => array("..."), "Classe P, NP" => array("..."), "Alg. Greedy" => array("..."));
 $_SESSION['NumIndex'] = $NoteCos;
 
 print_r($_SESSION['NumIndex']);
