d0600218aecb7e9e6a03d0adc7a3f7001228d29b
AndreaDagg <daguannoandrea@outlook.com>
2020-06-01 16:50:04 +0200

    PopolSec

--- Sicurezza/logicSicurezza.php
+++ Sicurezza/logicSicurezza.php
@@ -1,1 +1,31 @@
 <?php
+
+session_start();
+//Numero degli array degli argomenti considerando il proimo come zero
+$NumeroArgomenti = 1;
+$dbInformSec = array("");
+$dbAttack = array("");
+
+
+/* arg -> definisce l'argomento. Se non è specificato lo sceglie casualmente*/
+if (isset($_GET['arg'])) {
+    $dbNum = $_GET['arg'];
+} else {
+    $dbNum = rand(0, $NumeroArgomenti);
+}
+
+if (isset($dbNum)) {
+    switch ($dbNum) {
+        case 0:
+            $dbCo = $dbStruttureDati;
+            break;
+        case 1:
+            $dbCo = $dbGrafi;
+            break;
+
+    }
+}
+
+$r = rand(0, count($dbCo) - 1);
+$_SESSION ['result'] = json_encode($dbCo[$r]);
+header('Location: mainAlgoritmi.php');
--- Sicurezza/noteSicurezza.php
+++ Sicurezza/noteSicurezza.php
@@ -1,1 +1,16 @@
 <?php
+
+session_start();
+
+// Contiene le note che si trovano sotto i pulsanti del main
+
+
+$_SESSION['NumIndex'] = 0;
+$NotePass = array("");
+$NoteCos = array("Strutture Dati" => array("..."), "Grafi" => array("..."), "..." => array("..."), "..." => array("..."));
+$_SESSION['NumIndex'] = $NoteCos;
+
+print_r($_SESSION['NumIndex']);
+
+header("location: mainSicurezza.php ");
+
