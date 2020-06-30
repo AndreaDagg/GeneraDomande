b796641384a4b3bb8799a949ad4a18c11a187dce
AndreaDagg <daguannoandrea@outlook.com>
2020-06-30 22:48:55 +0200

    gui fix

--- Sicurezza/mainSicurezza.php
+++ Sicurezza/mainSicurezza.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['NumIndex']) || $_SESSION['NumIndex'] == 0) {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="styleSicurezza.css"
     <meta charset="UTF-8">
-    <link rel="icon" href="img/16.png" type="image/jpg"/>
+    <link rel="icon" href="../img/16.png" type="image/jpg"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
     <title>Generatore</title>
