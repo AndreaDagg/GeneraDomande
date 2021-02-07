6a8b5eee412a87f2d930ee9499a0644fb13435dc
AndreaDagg <daguannoandrea@outlook.com>
2021-02-07 21:08:44 +0100

    notlinear

--- Calcolo_Numerico/mainCalcolo.php
+++ Calcolo_Numerico/mainCalcolo.php
@@ -88,7 +88,7 @@ if (!isset($_SESSION['NumIndex']) || $_SESSION['NumIndex'] == 0) {
                 </div>
                 <div class="col">
                     <button id="btnARG" type="submit" class="btn btn-outline-secondary" name="arg" value="5"><h4>
-                            ... </h4>
+                            Sistemi non lineari </h4>
                     </button>
                 </div>
             </div>
