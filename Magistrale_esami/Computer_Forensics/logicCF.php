ed1ea9bb789f088658b118bb702310ed814f1a91
Andrea D'Aguanno <daguannoandrea@outlook.com>
2022-01-21 23:27:42 +0100

    das

--- Magistrale_esami/Computer_Forensics/logicCF.php
+++ Magistrale_esami/Computer_Forensics/logicCF.php
@@ -3,14 +3,14 @@
 session_start();
 $dbAcqui = array("CTU - CTP","Quali sono le modalita' di acquisizione di un dato?","Quali sono i vantaggi del sequestro?","Quando si ricorre all’isolamento di un dispositivo mobile?","Quali sono le tecniche di isolamento?","Quali sono i requisiti della duplicazione?","Quali sono i metodi di validazione di una duplicazione?","Come assicuriamo la conformita' di una duplicazione?","Quali sono le problematiche legate ad un sistema raid?","Quali sono le caratteristiche di una intercettazione?","Quando e' possibile utilizzare una porta monitor?","E’ possibile utilizzare nell’intercettazione l’attacco “Man In The Middle”?","Quali sono i protocolli da disabilitare nella creazione di una sonda? Perche'?","Differenza tra reati attraverso internet e reati informatici","Locus commissi delicti");
 $dbMedia = array("Come è organizzato un HD?","Com'e' il movimento di una testina?","Come avviene la letturea di un HDD","MBR");
-$dbFileSys = array("Come si scrive e legge un dato più grande di un Cluster?", "Cos'è il flush","Cos'e' il bad cluster","bad sector","MFT","Cos'e' il file system","metadata","file system jurnaled","Strutture della FAT","\$BITMAP");
+$dbFileSys = array("Come si scrive e legge un dato più grande di un Cluster?", "Cos'è il flush","Cos'e' il bad cluster","bad cluster","MFT","Cos'e' il file system","metadata","file system jurnaled","Strutture della FAT","\$BITMAP");
 $dbAnalWin = array("Difficolta' analisi windows","quali sono le HIVE","SAM","thumbs - pagefile - hiberfile","directory in AppData");
 $dbMobile = array("IMEI","ICCID","Acquisime memoria esterna","Tipologia di acquisizione applicabili alla memoria interna","Organizzazione dei dati in un dispositivo mobile");
-$dbImage = array("");
+$dbImage = array("EMA");
 $dbIdeReati = array("Cosa compone un identità fisica e cosa quella digitale?","ID digitale cosa permette di fare? Dove la trovo?","Come distinguere i reati? E le pene?","Quale procura è compentente dei reati informatici?","Reato informaco per eccellenza?","Quali leggi identificano i reati informatici?","Addescamento online reato da quando?","Come distinguo i reati informatici?","IDentita' digitale sociologica e giuridica","aggravante furto identita'","reati informatici puri e spuri");
 $dbCarte = array("Cose'è e cosa compone una carta di credito?","Cos'è lo skimmer e phishing? Cosa li accomuna alle carte di credito?","clonazione di una carta?","cos'e' il criptolocker");
 $dbPed = array("Come si impedisce di vedere un sito illegale?","Leggi Ped","Leggi pirateria audiovisiva","Grooming","divulgazione, distribuzione file pedo","CNCPO");
-$dbDir = array("diritto d'autore 663 art 41", "divulgazione pena","CNAIPIC","attacchi informatici piu' importanti","Truffa nigeriana","phishing, trashing boxing","truffa bancomat - pos");
+$dbDir = array("diritto d'autore 633/41", "divulgazione pena","CNAIPIC","attacchi informatici piu' importanti","Truffa nigeriana","phishing, trashing boxing","truffa bancomat - pos");
 /* arg -> definisce l'argomento. Se non è specificato lo sceglie casualmente*/
 if (isset($_GET['arg'])) {
     $dbNum = $_GET['arg'];
