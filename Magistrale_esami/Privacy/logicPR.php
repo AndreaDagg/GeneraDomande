0558091056042cf4a66ebc199394e2d28dd2a41c
Andrea D'Aguanno <daguannoandrea@outlook.com>
2023-01-25 18:45:11 +0100

    privacy#4

--- Magistrale_esami/Privacy/logicPR.php
+++ Magistrale_esami/Privacy/logicPR.php
@@ -4,7 +4,7 @@ session_start();
 $dbGDPR = array("GDPR", "Date Pubblicazione ed entrata in vigore GDPR","Da cosa protegge i dati personali il GDPR?", "Nuove Terminologie e figure introduce il GDPR","Principio di Accountability","Chi è tenuto a provare l'accountability?","Principio di Compliance","Principio R.I.D","Differenza tra Compliance ed Accountability");
 $dbIdentita = array("Identita' anagrafica e digitale", "Cosa formano le identita digitali ed anagrafiche", "Quale delle due tipologie di intentita si puo' rubare facilemnte?");
 $dbDatoPersonale = array("Cos'e' un dato perosnale?","Identificare ed essere Identificati","Quali informazioni online rendono identificabile una persona", "Cos'e' il trattamento dei dati personali?","In quale D.Leg si introducono i dati sensibili?","Quali dati personli richiedono il consenso esplicito dell'interessato?","Ciclo del trattamento dei dait personali","Dati Biometrici, Genetici e Particolari","Casi in cui i dati pesonali possono essere trattati senza il consenso esplicito","Quali dati vengono trattati da un'alzienda?");
-$dbCookieAndData = array("CyberEducazione", "Sicurezza del dato", "Coockie", "Differenza tra cookie tecnico, anlaitico e di terzi", "Data Breach");
+$dbCookieAndData = array("CyberEducazione", "Sicurezza del dato", "Cookie", "Differenza tra cookie tecnico, anlaitico e di terzi", "Data Breach", "679/2016");
 /* arg -> definisce l'argomento. Se non è specificato lo sceglie casualmente*/
 if (isset($_GET['arg'])) {
     $dbNum = $_GET['arg'];
