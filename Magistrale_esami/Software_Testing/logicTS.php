f5c4d4d94f201979b06f3d5a5bc4119820101cef
Andrea D'Aguanno <daguannoandrea@outlook.com>
2023-03-16 12:33:14 +0100

    Testing

---
+++ Magistrale_esami/Software_Testing/logicTS.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+$dbGDPR = array("GDPR", "Date Pubblicazione ed entrata in vigore GDPR","Da cosa protegge i dati personali il GDPR?", "Nuove Terminologie e figure introduce il GDPR","Principio di Accountability","Chi è tenuto a provare l'accountability?","Principio di Compliance","Principio R.I.D","Differenza tra Compliance ed Accountability");
+$dbIdentita = array("Identita' anagrafica e digitale", "Cosa formano le identita digitali ed anagrafiche", "Quale delle due tipologie di intentita si puo' rubare facilemnte?");
+$dbDatoPersonale = array("Cos'e' un dato perosnale?","Identificare ed essere Identificati","Quali informazioni online rendono identificabile una persona", "Cos'e' il trattamento dei dati personali?","In quale D.Leg si introducono i dati sensibili?","Quali dati personli richiedono il consenso esplicito dell'interessato?","Ciclo del trattamento dei dait personali","Dati Biometrici, Genetici e Particolari","Casi in cui i dati pesonali possono essere trattati senza il consenso esplicito","Quali dati vengono trattati da un'alzienda?");
+$dbCookieAndData = array("CyberEducazione", "Sicurezza del dato", "Cookie", "Differenza tra cookie tecnico, anlaitico e di terzi", "Data Breach", "679/2016");
+/* arg -> definisce l'argomento. Se non è specificato lo sceglie casualmente*/
+if (isset($_GET['arg'])) {
+    $dbNum = $_GET['arg'];
+} else {
+    $dbNum = rand(0, 3);
+}
+
+if (isset($dbNum)) {
+    switch ($dbNum) {
+        case 0:
+            $dbCo = $dbGDPR;
+            break;
+        case 1:
+            $dbCo = $dbIdentita;
+            break;
+        case 2:
+            $dbCo = $dbDatoPersonale;
+            break;
+        /*case 3:
+            $dbCo = $dbDerivateII;
+            break;*/
+        case 3:
+            $dbCo = $dbCookieAndData;
+            break;
+        case 4:
+            $dbCo = $dbMobile;
+            break;
+        case 5:
+            $dbCo = $dbImage;
+            break;
+        case 6:
+            $dbCo = $dbIdeReati;
+            break;
+        case 7:
+            $dbCo = $dbCarte;
+            break;
+    }
+}
+
+$r = rand(0, count($dbCo) - 1);
+$_SESSION ['result'] = json_encode($dbCo[$r]);
+header('Location: mainTS.php');
