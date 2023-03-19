a05ebaac5cb091ae61395269ea211e636656e8e4
Andrea D'Aguanno <daguannoandrea@outlook.com>
2023-03-19 22:38:39 +0100

    Update: testing quest

--- Magistrale_esami/Software_Testing/logicTS.php
+++ Magistrale_esami/Software_Testing/logicTS.php
@@ -1,7 +1,22 @@
-    <?php
+<?php
 
 session_start();
-$dbTESTING = array();
+$dbTESTING = array(
+
+    "Che tipo di attività è il testing e da cosa è composto", "
+da quando si può iniziare a pensare al testing e perché ", "
+Quali sono le risorse del testing e chi svolge le prime attività di testing", "
+Cos'e'un Test Case o caso di test", "
+Cos'e' precondition", "
+cos'e' un input e com'e' importante sceglierlo", "
+Test suite e perché definirla", "
+Come dimostriamo la correttezza di un programma e la presenza di difetti, Dijkstra", "
+System Version", "
+Test Stub e Drive", "
+Gestione deio fallimenti - Fault handling"
+
+
+);
 $dbBLACKandWHITE = array();
 /* arg -> definisce l'argomento. Se non è specificato lo sceglie casualmente*/
 if (isset($_GET['arg'])) {
