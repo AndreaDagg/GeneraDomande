6fea2c1947adfbf9773dbaac41c306b0a6f9ad14
Andrea D'Aguanno <daguannoandrea@outlook.com>
2023-03-19 22:42:06 +0100

    Refactoring+: testing quest

--- Magistrale_esami/Software_Testing/logicTS.php
+++ Magistrale_esami/Software_Testing/logicTS.php
@@ -2,20 +2,17 @@
 
 session_start();
 $dbTESTING = array(
-
-    "Che tipo di attività è il testing e da cosa è composto", "
-da quando si può iniziare a pensare al testing e perché ", "
-Quali sono le risorse del testing e chi svolge le prime attività di testing", "
-Cos'e'un Test Case o caso di test", "
-Cos'e' precondition", "
-cos'e' un input e com'e' importante sceglierlo", "
-Test suite e perché definirla", "
-Come dimostriamo la correttezza di un programma e la presenza di difetti, Dijkstra", "
-System Version", "
-Test Stub e Drive", "
-Gestione deio fallimenti - Fault handling"
-
-
+    "Che tipo di attività è il testing e da cosa è composto",
+    "da quando si può iniziare a pensare al testing e perché ",
+    "Quali sono le risorse del testing e chi svolge le prime attività di testing",
+    "Cos'e'un Test Case o caso di test",
+    "Cos'e' precondition",
+    "cos'e' un input e com'e' importante sceglierlo",
+    "Test suite e perché definirla",
+    "Come dimostriamo la correttezza di un programma e la presenza di difetti, Dijkstra",
+    "System Version",
+    "Test Stub e Drive",
+    "Gestione deio fallimenti - Fault handling"
 );
 $dbBLACKandWHITE = array();
 /* arg -> definisce l'argomento. Se non è specificato lo sceglie casualmente*/
